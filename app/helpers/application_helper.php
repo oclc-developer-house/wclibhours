e48ffed82cb7984735f6d9e68c8a49fd4214ed02
meyerst <meyerst@oclc.org>
2014-02-06 20:14:32 -0500

    Adding application helpers for dates and days

---
+++ app/helpers/application_helper.php
@@ -0,0 +1,23 @@
+<?php
+
+function formatDateTimeAsDate($dateTime) 
+{
+    $dateTimeObject = new DateTime($dateTime, new DateTimeZone('America/New_York'));
+    return $dateTimeObject->format('F j, Y');  
+}
+
+function getDayOrder($dayOfWeek)
+{
+    $days = array(
+        'Sunday' => 1,
+        'Monday' => 2,
+        'Tuesday' => 3,
+        'Wednesday' => 4,
+        'Thursday' => 5,
+        'Friday' => 6,
+        'Saturday' => 7
+    );
+    return $days[$dayOfWeek];
+}
+
+?>
